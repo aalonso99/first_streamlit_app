67f34b2eca303d9b107114f1b60502dd50e49943
aalonso99 <aalonsomembrilla@gmail.com>
2022-04-25 16:24:20 +0200

    Update streamlit_app.py
    Add some text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit as sl
 
-sl.title("My Parents New Healty Diner")
+sl.title("My Mom\'s New Healty Diner")
+
+sl.header('Breakfast Menu')
+sl.text('Omega 3 & Blueberry Oatmeal')
+sl.text('Hard-Boiled Free-Range Egg')
