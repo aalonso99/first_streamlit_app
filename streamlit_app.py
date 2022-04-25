c308aa39b53ae44e95295f6cb4ed9d50e30727d8
aalonso99 <aalonsomembrilla@gmail.com>
2022-04-25 16:37:42 +0200

    Update streamlit_app.py
    Changes selected fruit displaying name

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ sl.text('🥑🍞 Avocado Toast')
 
 sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt").set_index('Fruit')
 
 # To select the desired fruits in the smoothie
 sl.multiselect("Pick some fruits", list(my_fruit_list.index))
